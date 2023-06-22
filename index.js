87f5980289a155bc77497cb0c3905bb08f95ba8d
Njeri Mugeshi <nicolemugeshi@gmail.com>
2023-06-22 14:30:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code here
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
